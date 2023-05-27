14fbe605d4f618ca328ffceff37abdd1cf3971f8
cozyGarage <e1500953@edu.vamk.fi>
2023-05-28 00:09:33 +0300

    js

--- js/2048.js
+++ js/2048.js
@@ -1,3 +1,15 @@
 const scoreElement = document.querySelector('.score');
 
-let board = new Board(4, 4);
+const board = document.querySelector(".game_board");
+  const size = 4; // Size of the board
+  let cells = []; // Array to hold the board cells
+
+  // Initialize the board
+  function initializeBoard() {
+    for (let i = 0; i < size; i++) {
+      cells[i] = [];
+      for (let j = 0; j < size; j++) {
+        cells[i][j] = 0;
+      }
+    }
+  }
